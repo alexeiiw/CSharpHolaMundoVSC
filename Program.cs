8e64c71b301050b3860e986c66702f751d9c49f3
alexeiiw <alexeiiw@gmail.com>
2020-06-10 18:37:48 -0600

    Cambios de hoy

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSharpHolaMundoVSC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = System.Environment.MachineName;
+            Console.WriteLine("Hello World! " + name);
+        }
+    }
+}
